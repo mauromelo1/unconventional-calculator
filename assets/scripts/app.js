e99469c3c4dbbb5b97674513a3241c298af6795e
Mauro Melo <mauromelo1@gmail.com>
2022-06-23 17:32:43 -0300

    adding a custom function

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,1 +1,12 @@
-alert('works!!!');
+const defaultResult = 0;
+let currentResult = defaultResult;
+
+
+function add(num1, num2){
+    const result = num1 + num2;
+}
+
+let calculationDescription = `(${defaultResult} + 10) * 3 / 2 - 1`;
+
+outputResult(currentResult, calculationDescription);
+
