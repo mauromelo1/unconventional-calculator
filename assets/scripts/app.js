b7042588aa24428169f8cc244659e602a0472a7a
Mauro Melo <mauromelo1@gmail.com>
2022-06-28 14:20:45 -0300

    update the add function

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,12 +1,16 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
-
-function add(num1, num2){
-    const result = num1 + num2;
+function getUserNumberInput(){
+    return parseInt(userInput.value);
 }
 
-let calculationDescription = `(${defaultResult} + 10) * 3 / 2 - 1`;
 
-outputResult(currentResult, calculationDescription);
+function add(){
+    const enteredNumber = getUserNumberInput();
+    const calcDescription = `${currentResult} + ${enteredNumber}`;
+    currentResult = currentResult + enteredNumber;
+    outputResult(currentResult, calcDescription);
+}
 
+addBtn.addEventListener('click', add);
